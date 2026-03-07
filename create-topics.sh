f907e409cb09c0c97c640469a8165f06096bb5c9
lenin <leninfitfreak@gmail.com>
2026-03-07 15:48:21 +0530

    chore(stable): add external Kafka KRaft compose and topic bootstrap script

---
+++ create-topics.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+set -eu
+
+BOOTSTRAP="${KAFKA_BOOTSTRAP_SERVERS:-localhost:9092}"
+
+create_topic() {
+  topic="$1"
+  kafka-topics \
+    --bootstrap-server "$BOOTSTRAP" \
+    --create \
+    --if-not-exists \
+    --topic "$topic" \
+    --partitions 3 \
+    --replication-factor 1
+}
+
+create_topic "product-events"
+create_topic "order-events"
+create_topic "order-created"
+
+kafka-topics --bootstrap-server "$BOOTSTRAP" --list
